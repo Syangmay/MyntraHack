0727743358e57f341a59b284e90e92523c6e4856
ikoojoshi <ipshitamjoshi@gmail.com>
2020-11-01 15:07:21 +0530

    flaskjackets

--- app.py
+++ app.py
@@ -13,21 +13,94 @@ def index():
 def begin():
     return render_template("page1.html")
 
+
 @app.route('/gender')
 def gender():
     return render_template("gender.html")
+
+
 
 @app.route('/men')
 def men():
     return render_template("men.html")
 
+
+
 @app.route('/menjackets')
 def menjackets():
     return render_template("menjackets.html")
 
+@app.route('/menjackets2')
+def menjackets2():
+    return render_template("menjackets2.html")
+
+@app.route('/menjackets3')
+def menjackets3():
+    return render_template("menjackets3.html")
+
+@app.route('/menjackets4')
+def menjackets4():
+    return render_template("menjackets4.html")
+
+@app.route('/menjackets5')
+def menjackets5():
+    return render_template("menjackets5.html")
+
+@app.route('/menjackets6')
+def menjackets6():
+    return render_template("menjackets6.html")
+
+@app.route('/menjackets7')
+def menjackets7():
+    return render_template("menjackets7.html")
+
+@app.route('/menjackets8')
+def menjackets8():
+    return render_template("menjackets8.html")
+
+@app.route('/menjackets9')
+def menjackets9():
+    return render_template("menjackets9.html")
+
+@app.route('/menjackets10')
+def menjackets10():
+    return render_template("menjackets10.html")
+
+
+
+
 @app.route('/menjeans')
 def menjeans():
     return render_template("menjeans.html")
+
+@app.route('/menjeans1')
+def menjeans1():
+    return render_template("menjeans1.html")
+
+@app.route('/menjeans2')
+def menjeans2():
+    return render_template("menjeans2.html")
+
+@app.route('/menjeans3')
+def menjeans3():
+    return render_template("menjeans3.html")
+
+@app.route('/menjeans4')
+def menjeans4():
+    return render_template("menjeans4.html")
+
+@app.route('/menjeans5')
+def menjeans5():
+    return render_template("menjeans5.html")
+
+@app.route('/menjeans6')
+def menjeans6():
+    return render_template("menjeans6.html")
+
+@app.route('/menjeans7')
+def menjeans7():
+    return render_template("menjeans7.html")
+
 
 @app.route('/menshirt')
 def menshirt():
