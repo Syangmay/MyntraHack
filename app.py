ca66cbb0b67aafb61e136a382e1b952ccb6c11b4
ikoojoshi <ipshitamjoshi@gmail.com>
2020-11-01 14:04:48 +0530

    changing to flask

--- app.py
+++ app.py
@@ -9,5 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/begin')
+def begin():
+    return render_template("page1.html")
+
+@app.route('/gender')
+def gender():
+    return render_template("gender.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
